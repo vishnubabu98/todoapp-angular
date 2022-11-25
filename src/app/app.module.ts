95a9560904ddefb2a45359c2c05b4a13b16c4cb3
vishnu <vishnu@NTP-LAP-767.chn.nestgroup.net>
2022-11-25 12:39:30 +0530

    add some changes including navigations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,35 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddTodoComponent } from './add-todo/add-todo.component';
 import { ViewTodoComponent } from './view-todo/view-todo.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { Route, RouterModule, Routes } from '@angular/router';
+
+const myRoute:Routes=[
+  {
+    path:"",
+    component:AddTodoComponent
+  },
+  {
+    path:"viewtodo",
+    component:ViewTodoComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     AddTodoComponent,
-    ViewTodoComponent
+    ViewTodoComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
