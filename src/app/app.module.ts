681e0e295de2b207b56d18a28055201781407fb8
vishnu <vishnu@NTP-LAP-767.chn.nestgroup.net>
2022-11-25 12:28:42 +0530

    add viewtodo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddTodoComponent } from './add-todo/add-todo.component';
+import { ViewTodoComponent } from './view-todo/view-todo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddTodoComponent
+    AddTodoComponent,
+    ViewTodoComponent
   ],
   imports: [
     BrowserModule,
